028e8c7641f85076aca1b8c8f708f51434b21edf
Andres Gonzalez <andres.gonzalez42@hotmail.com>
2025-10-14 20:05:54 -0600

    Step 2

---
+++ KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/data/DefaultData.kt
@@ -0,0 +1,8 @@
+package io.github.kevinah95.data
+
+object DefaultData {
+    val sampleUsers = listOf(
+        User(id = "1", name = "Alice", email = "alice@gmail.com"),
+        User(id = "2", name = "Bob", email = "bob@gmail.com")
+    )
+}
---
+++ KMPKoin/shared/src/commonMain/kotlin/io/github/kevinah95/data/UserRepository.kt
@@ -0,0 +1,13 @@
+package io.github.kevinah95.data
+
+class UserRepository {
+    private val users = mutableListOf<User>()
+
+    fun addUser(user: User){
+        users.add(user)
+    }
+
+    fun getUsers() : List<User> = users
+
+
+}
